95c33dac71033ed57ec77d14c64b0e5d1fa4d23b
Heena164 <heenasr164@gmail.com>
2023-05-31 13:02:32 +0530

    Login User Page

--- djangoauthapi/account/views.py
+++ djangoauthapi/account/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from account.serializers import UserRegistrationSerializer
+from account.serializers import UserLoginSerializer
+
 
 # Create your views here.
+class UserRegistrationView(APIView):
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data = request.data)
+        if serializer.is_valid(raise_exception = True):
+            user = serializer.save()
+            return Response({'msg':'Registration Successful'}, status = status.HTTP_201_CREATED)
+
+        return Response({'msg':'Registration Successful'}, status = status.HTTP_400_BAD_REQUEST)
+ 
+class UserLoginView(APIView):
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data = request.data)
+        if serializer.is_valid(raise_exception = True):
+            email = serializer.data.get('email')
+            password = serializer.data.get('password')
+            user = authenticate(email = email, password = password)
+            if user is not None:
+                return Response({'msg': 'Login Success'}, status = status.HTTP_200_OK)
+
+            else:
+                return Response({'errors': {'non_fields_errors' : ['Email or Password is not valid']}}, status = status.HTTP_400_BAD_REQUEST)
+
+    
